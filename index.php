9d61a9693e90dc36890122eca88ba201fcd7c81a
kvalrie <hugo.barcelona31@gmail.com>
2017-08-20 23:03:25 +0200

    minichat.css added / minor css changes

--- index.php
+++ index.php
@@ -8,13 +8,25 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="minichat.css">
 	<meta charset="UTF-8">
 	<title>index</title>
 </head>
 <body>
-
-<a href="register.php"><button>REGISTER</button></a>
-<a href="login.php"><button>LOGIN</button></a>
+<div class="content_index">
+	<div class="img_register_login">
+		<img src="logo_footer_small.png" alt="lol">
+	</div>
+	
+	
+	
+	<div class="register_login">
+		<a href="register.php"><button class=button_index id="register">REGISTER</button></a>
+		<a href="login.php"><button class="button_index" id="login">LOGIN</button></a>
+	</div>
+</div>
 	
 </body>
 </html>
